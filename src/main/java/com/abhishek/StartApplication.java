c41b96935d4af5d70c615d25b5ed9e36afc2e990
Sidharth Shukla <90138886+sidharthhhh@users.noreply.github.com>
2023-09-21 12:55:53 +0530

    save

--- src/main/java/com/abhishek/StartApplication.java
+++ src/main/java/com/abhishek/StartApplication.java
@@ -13,41 +13,11 @@ public class StartApplication {
     @GetMapping("/")
     public String index(final Model model) {
         model.addAttribute("title", "This is a SpringBoot Static Web Application");
-        model.addAttribute("msg", "i m sidharth shukla, currently i m leaning devops. Today is day20 of my devops learning and today i learn about MAVEN and how to deploy Java based application through maven. If you like work then connect with me.
-                            list of command that i use are:-   1  sudo apt-get update -y
-    2  sudo apt install openjdk-11-jre -y
-    3  java -v
-    4  java -version
-    5  mvn -version
-    6  sudo apt-get install maven -y
-    7  git clone https://github.com/sidharthhhh/springboot-java-poject.git
-    8  cd springboot-java-poject/
-    9  ls
-   10  mvn validate
-   11  mvn compile
-   12  ls
-   13  cd target/
-   14  ls
-   15  cd ..
-   16  mvn test
-   17  mvn package
-   18  cd target/
-   19  ls
-   20  cd ..
-   21  mvn install
-   22  ls
-   23  mvn clear
-   24  ls
-   25  mvn clean package
-   26  ls
-   27  java -jar target/spring-boot-web.jar
-   28  history
-");
+        model.addAttribute("msg", "I'm Sidharth Shukla, currently I'm learning DevOps. Today is day 20 of my DevOps learning, and today I learned about MAVEN and how to deploy Java-based applications through Maven. If you'd like to work with me, connect with me.\nList of commands that I used:\n1. sudo apt-get update -y\n2. sudo apt install openjdk-11-jre -y\n3. java -v\n4. java -version\n5. mvn -version\n6. sudo apt-get install maven -y\n7. git clone https://github.com/sidharthhhh/springboot-java-project.git\n8. cd springboot-java-project/\n9. ls\n10. mvn validate\n11. mvn compile\n12. ls\n13. cd target/\n14. ls\n15. cd ..\n16. mvn test\n17. mvn package\n18. cd target/\n19. ls\n20. cd ..\n21. mvn install\n22. ls\n23. mvn clean\n24. ls\n25. mvn clean package\n26. ls\n27. java -jar target/spring-boot-web.jar\n28. history");
         return "index";
     }
 
     public static void main(String[] args) {
         SpringApplication.run(StartApplication.class, args);
     }
-
 }
