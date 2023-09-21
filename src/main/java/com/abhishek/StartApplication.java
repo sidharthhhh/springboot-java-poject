1fb2ca1516a792f9725749c203a83614b1912312
Sidharth Shukla <90138886+sidharthhhh@users.noreply.github.com>
2023-09-21 12:47:12 +0530

    Update StartApplication.java

--- src/main/java/com/abhishek/StartApplication.java
+++ src/main/java/com/abhishek/StartApplication.java
@@ -13,7 +13,7 @@ public class StartApplication {
     @GetMapping("/")
     public String index(final Model model) {
         model.addAttribute("title", "This is a SpringBoot Static Web Application");
-        model.addAttribute("msg", "Application Is Deployed To Kuberneets");
+        model.addAttribute("msg", "i m sidharth shukla, currently i m leaning devops. Today is day20 of my devops learning and today i learn about MAVEN and how to deploy Java based application through maven. If you like work then connect with me.");
         return "index";
     }
 
